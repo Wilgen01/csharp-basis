1a7fe0b4651fa700a8e5534bcccb6a841bae1ec7
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-02 19:43:48 -0500

    reverse string

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace csharp_basis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // reverse string
+            string text = "text to Invert";
+            string result = "";
+            int index = 0;
+            List<char> charList = new() { };
+            char[] charArray = new char[text.Length];
+
+            for (int i = text.Length - 1; i >= 0 ; i--)
+            {
+                result += text[i];
+                charList.Add(text[i]);
+                charArray[index] = text[i];
+                index++;
+            }
+
+            char[] textReversed = text.ToCharArray();
+            Array.Reverse(textReversed);
+            Console.WriteLine(result);
+            Console.WriteLine(string.Join("", charList));
+            Console.WriteLine(new string(charArray));
+            Console.WriteLine(new string(textReversed));
+
+
+
+
+        }
+    }
+}
