418205cdce8d6cbc5b6b9e15760c123e7ccb47c0
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-09 13:28:38 -0500

    probando linq

---
+++ Linq/Program.cs
@@ -0,0 +1,33 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Seleccionar paises con un numero de caracteres expecifico
+        List<String> paises = new() { "Colombia", "Venezuela", "Peru", "Ecuador", "Argentina", "Chile" };
+
+        getPaisesPorNumeroDeCaracteres(paises, 6, false);
+       
+
+    }
+
+    public static IEnumerable<String> getPaisesPorNumeroDeCaracteres(List<String> lista, int nCaracteres, bool print)
+    {
+        var result  = lista.Where(i => i.Length > nCaracteres);
+        if (print)
+        {
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+        
+
